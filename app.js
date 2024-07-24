fd47ecb08fa1b2f9c266285991b9d37f29dd5d06
Nitesh <ext_nkhanduja@falabella.cl>
2024-07-24 13:27:58 +0530

    methods in VUe

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const app = Vue.createApp({
     data(){
         return {
             firstName:'Nitesh',
-            lastName:"nitesh",
-            picture:"https://picsum.photos/200/300"
+            lastName:"Khanduja",
+            picture:"https://picsum.photos/200/300",
+            gender:"male"
+        }
+    },
+    methods:{
+        getUser(){
+            this.firstName='Nk',
+            this.lastName="kkr",
+            this.picture="https://picsum.photos/200/300",
+            this.gender="male"
         }
     }
 })
